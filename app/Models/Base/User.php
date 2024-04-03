16f43babca8a087a84f0abd9eb45224c5abaf974
Sammy Barasa <barsamms@gmail.com>
2024-04-03 10:45:53 +0300

    feat: added feature for model generation

--- app/Models/Base/User.php
+++ app/Models/Base/User.php
@@ -7,7 +7,7 @@
 namespace App\Models\Base;
 
 use Carbon\Carbon;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 
 /**
  * Class User
@@ -20,14 +20,25 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
  * @property string|null $remember_token
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
- *
  * @package App\Models\Base
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @mixin \Eloquent
  */
-class User extends Authenticatable
+class User extends Model
 {
-    protected $table = 'users';
+	protected $table = 'users';
 
-    protected $casts = [
-        'email_verified_at' => 'datetime'
-    ];
+	protected $casts = [
+		'email_verified_at' => 'datetime'
+	];
 }
--- app/Models/StaffType.php
+++ app/Models/StaffType.php
@@ -1,13 +1,39 @@
 <?php
 
+/**
+ * Created by Reliese Model.
+ */
+
 namespace App\Models;
 
-use App\Models\Base\StaffType as BaseStaffType;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
 
-class StaffType extends BaseStaffType
+/**
+ * Class StaffType
+ *
+ * @property int $id
+ * @property string $staff_type_name
+ * @property int $active
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @package App\Models
+ * @method static \Illuminate\Database\Eloquent\Builder|StaffType newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|StaffType newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|StaffType query()
+ * @method static \Illuminate\Database\Eloquent\Builder|StaffType whereActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|StaffType whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|StaffType whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|StaffType whereStaffTypeName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|StaffType whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class StaffType extends Model
 {
-	protected $hidden = [
-		'active'
+	protected $table = 'staff_type';
+
+	protected $casts = [
+		'active' => 'int'
 	];
 
 	protected $fillable = [
