09819b8cadde31200451ea46d4785c7140363b76
styx <barsamms@gmail.com>
2024-08-19 20:47:37 +0300

    feat: corrected data type

--- migrations/tables/m240819_170101_create_staff_type_table.php
+++ migrations/tables/m240819_170101_create_staff_type_table.php
@@ -4,17 +4,17 @@ use yii\db\Schema;
 
 class m240819_170101_create_staff_type_table extends \app\common\migration\BaseMigration
 {
-    public $tableName = '{{%staff_type}}';
+    public string $tableName = '{{%staff_type}}';
     public bool $addTimestamps = true;
 
     public function up()
     {
-        
+
         $this->createTable($this->tableName, [
             'id' => $this->bigPrimaryKey(),
             'name' => $this->string(20)->notNull(),
             'description' => $this->text()->notNull(),
-            'active' => $this->tinyint(1)->notNull()->defaultValue(1),
+            'active' => $this->boolean()->notNull()->defaultValue(1),
             'created_at' => $this->timestamp(),
             'updated_at' => $this->timestamp(),
         ], $this->tableOptions);
